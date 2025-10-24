2ce0833b852679fb2bba5cf78415e52365057a9d
Cillian <baugercillianpro@gmail.com>
2025-10-24 16:25:26 +0200

    buf fix

--- board.c
+++ board.c
@@ -6,6 +6,8 @@
 #define ROWS 10
 #define COLS 10
 
+static int win_position = -1;
+
 char *create_board(void) {
     char *tab_values = malloc(ROWS * COLS * sizeof(char));
     if (!tab_values) {
@@ -34,10 +36,22 @@ void display_board(char *tab_values) {
         {'#','#','#','#','#','#','#','#','#','#'}
     };
 
+    if (win_position == -1) {
+        for (int i = 0; i < ROWS * COLS; i++) {
+            if (tab_values[i] == '.') {
+                win_position = i;
+                break;
+            }
+        }
+    }
+
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
             int index = i * COLS + j;
-            if (tab_values[index] != ' ') {
+            
+            if (index == win_position && tab_values[index] != 'X' && tab_values[index] != 'o') {
+                board[i][j] = '.';
+            } else if (tab_values[index] != ' ') {
                 board[i][j] = tab_values[index];
             }
         }
@@ -137,25 +151,27 @@ void move_player(char *tab_values, char move) {
     }
 
     if (tab_values[next_pos] == 'X') {
+
         tab_values[after_next_pos] = 'X';
         tab_values[next_pos] = 'o';
         tab_values[player_pos] = ' ';
     } else {
+
         tab_values[next_pos] = 'o';
         tab_values[player_pos] = ' ';
     }
 }
 
 bool check_win(char *tab_values) {
-    for (int i = 0; i < ROWS * COLS; i++) {
-        if (tab_values[i] == '.') {
-            return false;
-        }
+
+    if (win_position != -1 && tab_values[win_position] == 'X') {
+        return true;
     }
-    return true;
+    return false;
 }
 
 bool check_lose(char *tab_values) {
+
     int box_pos = -1;
     for (int i = 0; i < ROWS * COLS; i++) {
         if (tab_values[i] == 'X') {
@@ -166,15 +182,25 @@ bool check_lose(char *tab_values) {
 
     if (box_pos == -1) return false;
 
+
+    if (box_pos == win_position) {
+        return false;
+    }
+
+    int row = box_pos / COLS;
+    int col = box_pos % COLS;
+
     int up = box_pos - COLS;
     int down = box_pos + COLS;
     int left = box_pos - 1;
     int right = box_pos + 1;
 
-    bool up_blocked = (up < 0 || tab_values[up] == '#');
-    bool down_blocked = (down >= ROWS * COLS || tab_values[down] == '#');
-    bool left_blocked = (left < 0 || tab_values[left] == '#');
-    bool right_blocked = (right >= ROWS * COLS || tab_values[right] == '#');
+
+    bool up_blocked = (row <= 1 || tab_values[up] == '#');
+    bool down_blocked = (row >= ROWS - 2 || tab_values[down] == '#');
+    bool left_blocked = (col <= 1 || tab_values[left] == '#');
+    bool right_blocked = (col >= COLS - 2 || tab_values[right] == '#');
+
 
     if ((up_blocked && left_blocked) || (up_blocked && right_blocked) ||
         (down_blocked && left_blocked) || (down_blocked && right_blocked)) {
@@ -195,7 +221,11 @@ void save_board(char *tab_values) {
         for (int j = 0; j < COLS; j++) {
             int index = i * COLS + j;
             char c = tab_values[index];
-            if (c == ' ') {
+            
+
+            if (index == win_position && c == ' ') {
+                fputc('.', file);
+            } else if (c == ' ') {
                 if (i == 0 || i == ROWS - 1 || j == 0 || j == COLS - 1) {
                     fputc('#', file);
                 } else {
@@ -213,4 +243,5 @@ void save_board(char *tab_values) {
 
 void free_board(char *tab_values) {
     free(tab_values);
+    win_position = -1;
 }
